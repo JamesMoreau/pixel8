26afced383443a32c84708761674a4be18aae935
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2024-10-06 02:59:43 -0400

    Refactor pixel8 function to use blockSize parameter for resizing

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/color"
 	"image/jpeg"
 	"image/png"
 	"log"
@@ -14,9 +15,21 @@ import (
 
 /*
 TODO
--Grayscale
--Color Palettes
+-should i always start for loops from 0 if img rectangle doesn't necessarily start at 0.
 */
+
+var (
+	nesPalette = []color.Color {
+		color.RGBA{0x80, 0x80, 0x80, 0xFF}, // Gray
+		color.RGBA{0x00, 0x00, 0xFF, 0xFF}, // Blue
+		color.RGBA{0x00, 0xFF, 0x00, 0xFF}, // Green
+		color.RGBA{0xFF, 0x00, 0x00, 0xFF}, // Red
+		color.RGBA{0xFF, 0xFF, 0x00, 0xFF}, // Yellow
+		color.RGBA{0xFF, 0xA5, 0x00, 0xFF}, // Orange
+		color.RGBA{0x00, 0x00, 0x00, 0xFF}, // Black
+		color.RGBA{0xFF, 0xFF, 0xFF, 0xFF}, // White
+	}
+)
 
 func main() {
 	img, err := openImage("assets/asuka.jpeg")
@@ -24,12 +37,27 @@ func main() {
 		log.Fatal(err)
 	}
 
-	blockSize := 16
-	pixelated := pixel8(img, blockSize)
-	err = saveImageToFile(pixelated, "output/asuka.jpg")
+	pixel8ed := processPixel8(img, 8, false, nesPalette)
+
+	err = saveImageToFile(pixel8ed, "output/asuka.jpg")
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func processPixel8(img image.Image, blockSize int, grayscale bool, palette []color.Color) image.Image {
+	pixel8ed := pixel8(img, blockSize)
+	
+	if grayscale {
+		return convertToGrayscale(pixel8ed)
+	}
+	
+	usePalette := palette != nil
+	if usePalette {
+		return convertToColorPalette(pixel8ed, palette)
+	}
+
+	return pixel8ed
 }
 
 func openImage(filename string) (image.Image, error) {
@@ -108,4 +136,54 @@ func saveImageToFile(img image.Image, filepath string) error {
 	}
 
 	return nil
+}
+
+func convertToColorPalette(img image.Image, palette []color.Color) image.Image {
+	bounds := img.Bounds()
+	paletteImg := image.NewRGBA(bounds)
+	for y := 0; y < bounds.Dy(); y++ {
+		for x := 0; x < bounds.Dx(); x++ {
+			originalColor := color.RGBAModel.Convert(img.At(x, y)).(color.RGBA)
+			closestColor := findClosestPaletteColor(originalColor, palette)
+
+			paletteImg.Set(x, y, closestColor)
+		}
+	}
+
+	return paletteImg
+}
+
+func findClosestPaletteColor(c color.Color, palette []color.Color) color.Color {
+	closestColor := palette[0]
+	minDistance := math.MaxFloat64
+
+	for _, p := range palette {
+		pr, pg, pb, _ := p.RGBA()
+		cr, cg, cb, _ := c.RGBA()
+		
+		dr := float64(cr>>8) - float64(pr>>8)
+		dg := float64(cg>>8) - float64(pg>>8)
+		db := float64(cb>>8) - float64(pb>>8)
+
+		distance := dr*dr + dg*dg + db*db
+		if distance < minDistance {
+			minDistance = distance
+			closestColor = p
+		}
+	}
+
+	return closestColor
+}
+
+// This grayscale converter uses the "Average" method.
+func convertToGrayscale(img image.Image) image.Image {
+	bounds := img.Bounds()
+	gray := image.NewRGBA(bounds)
+
+
+	for x := bounds.Dx(); x < bounds.Dx(); x++ {
+
+	}
+
+	return gray
 }
